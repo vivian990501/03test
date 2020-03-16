31aa47aa2f23e0c0c1deb9b7bee68182051d5cc5
Chia Hsin Wu <gill861111@gmail.com>
2020-03-16 14:52:50 +0800

    我做好計算機按鈕了!! 很棒吧!

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,15 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
